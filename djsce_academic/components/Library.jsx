27b07ac4cf608e10e06f4d84fd91243e267bf3e7
Nv31616 <nishantv10092005@gmail.com>
2026-05-10 21:35:00 +0530

    Added id to each rack & block

--- djsce_academic/components/Library.jsx
+++ djsce_academic/components/Library.jsx
@@ -56,145 +56,147 @@ const Library = () => {
               <WoodenRack
                 side="LH"
                 number="13"
+                id = "wdn-lh-13"
                 alignment="top-full left-0 mt-2"
-                ref={(el) => (rackRefs.current["LH-13"] = el)}
+                ref={(el) => (rackRefs.current["wdn-lh-13"] = el)}
               />
               <WoodenRack
                 side="LH"
                 number="12"
+                id = "wdn-lh-12"
                 alignment="top-full left-0 mt-2"
-                ref={(el) => (rackRefs.current["LH-12"] = el)}
+                ref={(el) => (rackRefs.current["wdn-lh-12"] = el)}
               />
             </div>
 
             <div className="col-span-1" />
 
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="B" number="12" ref={(el) => (rackRefs.current["B-12"] = el)}/>
-              <SteelBlock side="F" number="12" ref={(el) => (rackRefs.current["F-12"] = el)}/>
+              <SteelBlock id="stl-b-12" side="B" number="12" ref={(el) => (rackRefs.current["stl-b-12"] = el)}/>
+              <SteelBlock id="stl-f-12" side="F" number="12" ref={(el) => (rackRefs.current["stl-f-12"] = el)}/>
             </div>
 
             <div className="col-span-3" />
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="F" number="11" ref={(el) => (rackRefs.current["F-11"] = el)}/>
-              <SteelBlock side="B" number="11" ref={(el) => (rackRefs.current["B-11"] = el)}/>
+              <SteelBlock id="stl-f-11" side="F" number="11" ref={(el) => (rackRefs.current["stl-f-11"] = el)}/>
+              <SteelBlock id="stl-b-11" side="B" number="11" ref={(el) => (rackRefs.current["stl-b-11"] = el)}/>
             </div>
 
             <div className="col-span-1" />
 
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="RH" number="13" ref={(el) => (rackRefs.current["RH-13"] = el)}/>
-              <WoodenRack side="RH" number="12" ref={(el) => (rackRefs.current["RH-12"] = el)} />
+              <WoodenRack id="wdn-rh-13" side="RH" number="13" ref={(el) => (rackRefs.current["wdn-rh-13"] = el)}/>
+              <WoodenRack id="wdn-rh-12" side="RH" number="12" ref={(el) => (rackRefs.current["wdn-rh-12"] = el)} />
             </div>
 
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="LH" number="11" ref={(el) => (rackRefs.current["LH-11"] = el)}/>
-              <WoodenRack side="LH" number="10" ref={(el) => (rackRefs.current["LH-10"] = el)}/>
-            </div>
-
-            <div className="col-span-1" />
-
-            <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="B" number="10" ref={(el) => (rackRefs.current["B-10"] = el)}/>
-              <SteelBlock side="F" number="10" ref={(el) => (rackRefs.current["F-10"] = el)}/>
-            </div>
-
-            <div className="col-span-3" />
-
-            <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="F" number="9" ref={(el) => (rackRefs.current["F-9"] = el)}/>
-              <SteelBlock side="B" number="9" ref={(el) => (rackRefs.current["B-9"] = el)}/>
-            </div>
-
-            <div className="col-span-1" />
-
-            <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="RH" number="11" ref={(el) => (rackRefs.current["RH-11"] = el)}/>
-              <WoodenRack side="RH" number="10" ref={(el) => (rackRefs.current["RH-10"] = el)}/>
-            </div>
-
-            <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="LH" number="9" ref={(el) => (rackRefs.current["LH-9"] = el)}/>
-              <WoodenRack side="LH" number="8" ref={(el) => (rackRefs.current["LH-8"] = el)}/>
+              <WoodenRack id="wdn-lh-11" side="LH" number="11" ref={(el) => (rackRefs.current["wdn-lh-11"] = el)}/>
+              <WoodenRack id="wdn-lh-10" side="LH" number="10" ref={(el) => (rackRefs.current["wdn-lh-10"] = el)}/>
             </div>
 
             <div className="col-span-1" />
 
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="B" number="8" ref={(el) => (rackRefs.current["B-8"] = el)}/>
-              <SteelBlock side="F" number="8" ref={(el) => (rackRefs.current["F-8"] = el)}/>
+              <SteelBlock id="stl-b-10" side="B" number="10" ref={(el) => (rackRefs.current["stl-b-10"] = el)}/>
+              <SteelBlock id="stl-f-10" side="F" number="10" ref={(el) => (rackRefs.current["stl-f-10"] = el)}/>
             </div>
 
             <div className="col-span-3" />
 
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="F" number="7" ref={(el) => (rackRefs.current["F-7"] = el)}/>
-              <SteelBlock side="B" number="7" ref={(el) => (rackRefs.current["B-7"] = el)}/>
+              <SteelBlock id="stl-f-9" side="F" number="9" ref={(el) => (rackRefs.current["stl-f-9"] = el)}/>
+              <SteelBlock id="stl-b-9" side="B" number="9" ref={(el) => (rackRefs.current["stl-b-9"] = el)}/>
+            </div>
+
+            <div className="col-span-1" />
+
+            <div className="col-span-3 flex h-16 flex-col">
+              <WoodenRack id="wdn-rh-11" side="RH" number="11" ref={(el) => (rackRefs.current["wdn-rh-11"] = el)}/>
+              <WoodenRack id="wdn-rh-10" side="RH" number="10" ref={(el) => (rackRefs.current["wdn-rh-10"] = el)}/>
+            </div>
+
+            <div className="col-span-3 flex h-16 flex-col">
+              <WoodenRack id="wdn-lh-9" side="LH" number="9" ref={(el) => (rackRefs.current["wdn-lh-9"] = el)}/>
+              <WoodenRack id="wdn-lh-8" side="LH" number="8" ref={(el) => (rackRefs.current["wdn-lh-8"] = el)}/>
+            </div>
+
+            <div className="col-span-1" />
+
+            <div className="col-span-1 -mt-8 mb-24 flex h-32">
+              <SteelBlock id="stl-b-8" side="B" number="8" ref={(el) => (rackRefs.current["stl-b-8"] = el)}/>
+              <SteelBlock id="stl-f-8" side="F" number="8" ref={(el) => (rackRefs.current["stl-f-8"] = el)}/>
+            </div>
+
+            <div className="col-span-3" />
+
+            <div className="col-span-1 -mt-8 mb-24 flex h-32">
+              <SteelBlock id="stl-f-7" side="F" number="7" ref={(el) => (rackRefs.current["stl-f-7"] = el)}/>
+              <SteelBlock id="stl-b-7" side="B" number="7" ref={(el) => (rackRefs.current["stl-b-7"] = el)}/>
             </div>
 
             <div className="col-span-1" />
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="RH" number="9" ref={(el) => (rackRefs.current["RH-9"] = el)}/>
-              <WoodenRack side="RH" number="8" ref={(el) => (rackRefs.current["RH-8"] = el)}/>
+              <WoodenRack id="wdn-rh-9" side="RH" number="9" ref={(el) => (rackRefs.current["wdn-rh-9"] = el)}/>
+              <WoodenRack id="wdn-rh-8" side="RH" number="8" ref={(el) => (rackRefs.current["wdn-rh-8"] = el)}/>
             </div>
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="LH" number="7" ref={(el) => (rackRefs.current["LH-7"] = el)}/>
-              <WoodenRack side="LH" number="6" ref={(el) => (rackRefs.current["LH-6"] = el)}/>
+              <WoodenRack id="wdn-lh-7" side="LH" number="7" ref={(el) => (rackRefs.current["wdn-lh-7"] = el)}/>
+              <WoodenRack id="wdn-lh-6" side="LH" number="6" ref={(el) => (rackRefs.current["wdn-lh-6"] = el)}/>
             </div>
             <div className="col-span-1" />
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="B" number="6" ref={(el) => (rackRefs.current["B-6"] = el)}/>
-              <SteelBlock side="F" number="6" ref={(el) => (rackRefs.current["F-6"] = el)}/>
+              <SteelBlock id="stl-b-6" side="B" number="6" ref={(el) => (rackRefs.current["stl-b-6"] = el)}/>
+              <SteelBlock id="stl-f-6" side="F" number="6" ref={(el) => (rackRefs.current["stl-f-6"] = el)}/>
             </div>
 
             <div className="col-span-3" />
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="F" number="5" ref={(el) => (rackRefs.current["F-5"] = el)}/>
-              <SteelBlock side="F" number="5" ref={(el) => (rackRefs.current["F-5"] = el)}/>
+              <SteelBlock id="stl-f-5" side="F" number="5" ref={(el) => (rackRefs.current["F-5"] = el)}/>
+              <SteelBlock id="stl-b-6" side="B" number="5" ref={(el) => (rackRefs.current["b-5"] = el)}/>
             </div>
 
             <div className="col-span-1" />
 
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="RH" number="7" ref={(el) => (rackRefs.current["RH-7"] = el)}/>
-              <WoodenRack side="RH" number="6" ref={(el) => (rackRefs.current["RH-6"] = el)}/>
+              <WoodenRack id="wdn-rh-7" side="RH" number="7" ref={(el) => (rackRefs.current["wdn-rh-7"] = el)}/>
+              <WoodenRack id="wdn-rh-6" side="RH" number="6" ref={(el) => (rackRefs.current["wdn-rh-6"] = el)}/>
             </div>
 
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="LH" number="5" ref={(el) => (rackRefs.current["LH-5"] = el)}/>
-              <WoodenRack side="LH" number="4" ref={(el) => (rackRefs.current["LH-4"] = el)}/>
+              <WoodenRack id="wdn-lh-5" side="LH" number="5" ref={(el) => (rackRefs.current["wdn-lh-5"] = el)}/>
+              <WoodenRack id="wdn-lh-4" side="LH" number="4" ref={(el) => (rackRefs.current["wdn-lh-4"] = el)}/>
             </div>
 
             <div className="col-span-1" />
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="B" number="4" ref={(el) => (rackRefs.current["B-5"] = el)}/>
-              <SteelBlock side="F" number="4" ref={(el) => (rackRefs.current["F-4"] = el)}/>
+              <SteelBlock id="stl-f-4" side="B" number="4" ref={(el) => (rackRefs.current["stl-b-4"] = el)}/>
+              <SteelBlock id="stl-b-4" side="F" number="4" ref={(el) => (rackRefs.current["stl-f-4"] = el)}/>
             </div>
 
             <div className="col-span-3" />
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="F" number="3" ref={(el) => (rackRefs.current["F-3"] = el)}/>
-              <SteelBlock side="B" number="3" ref={(el) => (rackRefs.current["B-3"] = el)}/>
+              <SteelBlock id="stl-f-3" side="F" number="3" ref={(el) => (rackRefs.current["stl-f-3"] = el)}/>
+              <SteelBlock id="stl-b-3" side="B" number="3" ref={(el) => (rackRefs.current["stl-b-3"] = el)}/>
             </div>
 
             <div className="col-span-1" />
             <div className="col-span-3 flex h-16 flex-col">
-              <WoodenRack side="RH" number="5" ref={(el) => (rackRefs.current["RH-5"] = el)}/>
-              <WoodenRack side="RH" number="4" ref={(el) => (rackRefs.current["RH-4"] = el)}/>
+              <WoodenRack id="wdn-rh-5" side="RH" number="5" ref={(el) => (rackRefs.current["wdn-rh-5"] = el)}/>
+              <WoodenRack id="wdn-rh-4" side="RH" number="4" ref={(el) => (rackRefs.current["wdn-rh-4"] = el)}/>
               <Computers />
             </div>
             <div className="col-span-3 flex flex-col">
               <div className="col-span-3 flex h-16 flex-col">
-                <WoodenRack side="LH" number="3" ref={(el) => (rackRefs.current["LH-3"] = el)}/>
-                <WoodenRack side="LH" number="2" ref={(el) => (rackRefs.current["LH-2"] = el)}/>
+                <WoodenRack id="wdn-lh-3" side="LH" number="3" ref={(el) => (rackRefs.current["wdn-lh-3"] = el)}/>
+                <WoodenRack id="wdn-lh-2" side="LH" number="2" ref={(el) => (rackRefs.current["wdn-lh-2"] = el)}/>
               </div>
               <Computers />
-              <WoodenRack side="LH" number="1" ref={(el) => (rackRefs.current["LH-1"] = el)}/>
+              <WoodenRack od="wdn-lh-1" side="LH" number="1" ref={(el) => (rackRefs.current["wdn-lh-1"] = el)}/>
             </div>
             <div className="col-span-1" />
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="B" number="2" ref={(el) => (rackRefs.current["B-2"] = el)}/>
-              <SteelBlock side="F" number="2" ref={(el) => (rackRefs.current["F-2"] = el)}/>
+              <SteelBlock id="stl-b-2" side="B" number="2" ref={(el) => (rackRefs.current["stl-b-2"] = el)}/>
+              <SteelBlock id="stl-f-2" side="F" number="2" ref={(el) => (rackRefs.current["stl-f-2"] = el)}/>
             </div>
             <div className="col-span-3 flex items-end">
               <div className="backdrop-blur-xl w-full flex h-1/3 border border-[#875003] flex-col items-center justify-between p-1">
@@ -206,8 +208,8 @@ const Library = () => {
             </div>
 
             <div className="col-span-1 -mt-8 mb-24 flex h-32">
-              <SteelBlock side="F" number="1" ref={(el) => (rackRefs.current["F-1"] = el)}/>
-              <SteelBlock side="B" number="1" ref={(el) => (rackRefs.current["B-1"] = el)}/>
+              <SteelBlock id="stl-f-1" side="F" number="1" ref={(el) => (rackRefs.current["stl-f-1"] = el)}/>
+              <SteelBlock id="stl-b-1" side="B" number="1" ref={(el) => (rackRefs.current["stl-b-1"] = el)}/>
             </div>
 
             <div className="col-span-1 -ml-10 flex h-full flex-col items-center justify-end pb-0">
@@ -220,11 +222,11 @@ const Library = () => {
 
             <div className="col-span-3 flex flex-col justify-between">
               <div className="col-span-3 flex h-16 flex-col">
-                <WoodenRack side="RH" number="3" ref={(el) => (rackRefs.current["RH-3"] = el)}/>
-                <WoodenRack side="RH" number="2" ref={(el) => (rackRefs.current["RH-2"] = el)}/>
+                <WoodenRack id="wdn-rh-3" side="RH" number="3" ref={(el) => (rackRefs.current["wdn-rh-3"] = el)}/>
+                <WoodenRack id="wdn-rh-2" side="RH" number="2" ref={(el) => (rackRefs.current["wdn-rh-2"] = el)}/>
               </div>
               <div className="flex items-center justify-end">
-                <WoodenRack side="RH" number="1" ref={(el) => (rackRefs.current["RH-1"] = el)}/>
+                <WoodenRack id="wdn-rh-1" side="RH" number="1" ref={(el) => (rackRefs.current["wdn-rh-1"] = el)}/>
               </div>
             </div>
           </div>
--- djsce_academic/components/SearchCard.jsx
+++ djsce_academic/components/SearchCard.jsx
@@ -9,30 +9,48 @@ const SearchCard = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
   const { setHighlightedId } = useContext(LibraryContext);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
+  // 1. Calculate the slice of books to show
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = books.slice(indexOfFirstItem, indexOfLastItem);
+
+  // 2. Calculate total pages
+  const totalPages = Math.ceil(books.length / itemsPerPage);
+
+  // SearchCard.jsx
   useEffect(() => {
-    if(query===""){
-        setBooks([])
-        return;
+    // 1. Don't search if the query is empty
+    if (!query.trim()) {
+      setBooks([]);
+      return;
     }
+
     const performSearch = async () => {
       setLoading(true);
       try {
-        const response = await fetch(`http://127.0.0.1:8000/api/search/?q=${query}`);
+        const response = await fetch(
+          `http://127.0.0.1:8000/api/search/?q=${query}`,
+        );
         const data = await response.json();
-        setBooks(data.books);
+        setBooks(data.books || []);
       } catch (error) {
         console.error("Search failed:", error);
       } finally {
         setLoading(false);
       }
-
-      const timeoutId = setTimeout(() => {
-        performSearch();
-      } , 300);
-      return () => clearTimeout(timeoutId);
     };
-  }, [query]);
+
+    // 2. THIS IS THE MISSING PART: Actually trigger the function
+    const timeoutId = setTimeout(() => {
+      performSearch();
+    }, 300);
+
+    // 3. Cleanup the timer if the user types again
+    return () => clearTimeout(timeoutId);
+  }, [query]); // Re-run when query changes
 
   return (
     <div className="w-1/3 bg-[#dbc397] my-3 ml-2 mr-3 rounded-2xl p-6 flex flex-col border-amber-900 border-2">
@@ -43,13 +61,19 @@ const SearchCard = () => {
         type="text"
         id="book-search"
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          setCurrentPage(1); // Reset to page 1 when searching
+        }}
         placeholder="Enter Book Title or Topic..."
         className="w-full p-3 border-2 border-amber-700 text-xl text-amber-900 font-bold font-mono rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-900"
       />
       <div id="results-list" className="mt-6 flex-1 overflow-y-auto space-y-3">
-        {books.map((book) => (
-          <div className="p-4 border bg-amber-100 border-amber-900 rounded-2xl">
+        {currentItems.map((book, i) => (
+          <div
+            key={i}
+            className="p-4 border bg-amber-100 border-amber-900 rounded-2xl"
+          >
             <p className="font-bold text-xl text-amber-900">{book.title}</p>
             <p className="text-s text-[#ba3e1f] mb-3">{`${book.type} ${book.number} | ${book.side}`}</p>
             <button
@@ -61,6 +85,28 @@ const SearchCard = () => {
           </div>
         ))}
       </div>
+      {/* Pagination controls */}
+      {books.length > itemsPerPage && (
+        <div className="flex justify-between m-4">
+          <button
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+            className="disabled:opacity-30 hover:scale-105 cursor-pointer bg-[#B83D1E] border-2 border-[#B83D1E] font-mono hover:bg-white hover:text-[#B83D1E] rounded-3xl w-30 h-10 text-white"
+          >
+            Previous
+          </button>
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            className="disabled:opacity-30 hover:scale-105 cursor-pointer bg-[#B83D1E] border-2 border-[#B83D1E] font-mono hover:bg-white hover:text-[#B83D1E] rounded-3xl w-30 h-10 text-white"
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 };
